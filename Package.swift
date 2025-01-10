18fd1c98473510d41ab1394bfe006a215ca035f1
Dmitriy Tyagniy <d.tyagniy@corp.mail.ru>
2025-01-10 14:18:09 +0300

    Release 2.8.3

--- Package.swift
+++ Package.swift
@@ -19,12 +19,12 @@ let package = Package(
     targets: [
         Target.binaryTarget(name: "Libverify",
                             url: "https://github.com/notifySDK/notify-sdk-ios/releases/download/v2.8.3/Libverify.zip",
-                            checksum: "7b203c40403bb5d196b17bc8382a93d536b95603aca53d3bd3d9b909dd673470"),
+                            checksum: "543d7be62f9b7cde23d843f80b242a67d07a0a255801b9d39a517ca083062673"),
         Target.binaryTarget(name: "LibverifyControls",
                             url: "https://github.com/notifySDK/notify-sdk-ios/releases/download/v2.8.3/LibverifyControls.zip",
-                            checksum: "450e7e9880e8523a3bfd72d52b3e219ccf1ce86e4ebed7ca9078b223396aef02"),
+                            checksum: "1f5252e59f24d96a17a809263a076b25b726bcbbed3550a0a3e0fcd3dbe095ea"),
         Target.binaryTarget(name: "LibverifyExt",
                             url: "https://github.com/notifySDK/notify-sdk-ios/releases/download/v2.8.3/LibverifyExt.zip",
-                            checksum: "98b84bb531d8be321d6b43b0c5a4a315505041723d4b1b34234ca24211cc2257")
+                            checksum: "1ab51c9b2841883e6de4db7bc8872fdca622deaa9cb765eb14908ea4729b94e5")
     ]
 )
