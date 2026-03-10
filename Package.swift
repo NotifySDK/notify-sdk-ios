a9e741df65b4ce555b32bc49078f233abae3803e
NotifySDK CI <ci-bot@notify-sdk.com>
2026-03-10 13:30:44 +0300

    Release 2.9.10

--- Package.swift
+++ Package.swift
@@ -15,27 +15,27 @@ let package = Package(
         .binaryTarget(
             name: "Libverify",
             url: "https://github.com/notifySDK/notify-sdk-ios/releases/download/v2.9.10/Libverify.zip",
-            checksum: "32de22449354b74a3b3c1f4de4344046ee2bc2457bb2b112576c57e13e788794"
+            checksum: "2d9b758a579d87196d460899d625c56f30017a158036a8c8c71520d12cb74a78"
         ),
         .binaryTarget(
             name: "LibverifyControls",
             url: "https://github.com/notifySDK/notify-sdk-ios/releases/download/v2.9.10/LibverifyControls.zip",
-            checksum: "176898dfc9d8dad3522d23223cb2221db1e65da5509f93a6790d4e33b383ed1c"
+            checksum: "7ff07483dce7d98c5d9c864dcba7832c2aa25b488d27d2ae94e3b5379e824a77"
         ),
         .binaryTarget(
             name: "LibverifyExt",
             url: "https://github.com/notifySDK/notify-sdk-ios/releases/download/v2.9.10/LibverifyExt.zip",
-            checksum: "666d86cee8b3b2911510413bd86cbeb67866e050adb640650ba5e55196b6515c"
+            checksum: "780bf122462ef74eee84fbdac65687c57552e0541d8ea749be3cb3e5b0bbd36f"
         ),
         .binaryTarget(
             name: "LibverifyNoTracer",
             url: "https://github.com/notifySDK/notify-sdk-ios/releases/download/v2.9.10/Libverify-NoTracer.zip",
-            checksum: "9bc0a3391bb9da747ecbb7abf789d86f1a7b887c24ef19225b0c0cbced346e2b"
+            checksum: "d08da63cf36048c455e038b6dc85a143ef708e6dd2a12e5581cfd4141254a1b2"
         ),
         .binaryTarget(
             name: "LibverifyControlsNoTracer",
             url: "https://github.com/notifySDK/notify-sdk-ios/releases/download/v2.9.10/LibverifyControls-NoTracer.zip",
-            checksum: "2b432226156a4151514874abf595c4713752933fcb7ff065b790a43f0c61fb1e"
+            checksum: "6fb28b3c8da80b12900fe35030720df9ad5952d34769f621cdf6c44bd9293d2b"
         )
     ]
 )
